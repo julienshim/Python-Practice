7c4cc78374290e8c30ae8a749d2511d2c0131622
jerauld <25723428+jerauld@users.noreply.github.com>
2018-08-16 18:15:17 -0700

    ex3

---
+++ ex3.py
@@ -0,0 +1,37 @@
+#prints string
+print("I will now count my chickens:")
+
+#prints string, then OOO 25 + 5 = 30
+print("Hens", float(25 + 30 / 6))
+#prints string, then OOO 100 - 75 % 4 --> 100 - 3
+print("Roosters", float(100 - 25 * 3 % 4))
+
+#prints string
+print("Now I will count the eggs:")
+
+#prints OOO 6 - 5 + 0 - .25 + 6 --> 1 -.25 + 6 - .75 + 6
+print(float(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6))
+
+#prints string
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#5 < -2  --> false
+print(float(3 + 2 < 5 - 7))
+
+#prints string then answer 5
+print("What is 3 + 2?", float(3 + 2))
+#prints string then answer -2
+print("What is 5 - 7?", float(5 - 7))
+
+#prints string
+print("Oh, that's why it's False.")
+
+#prints string
+print("How about some more.")
+
+#prints string, then true
+print("Is it greater?", float(5 > -2))
+#prints string, then true
+print("Is it greater or equal?", float(5 >= - 2))
+#prints string, then false
+print("Is it less or equal?", float(5 <= -2))
