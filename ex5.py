466709bead178e873e069abab5a2eef7eb54a3ed
jerauld <25723428+jerauld@users.noreply.github.com>
2018-08-16 18:47:01 -0700

    added metric variants

--- ex5.py
+++ ex5.py
@@ -9,8 +9,10 @@ teeth = 'White'
 hair = 'Black'
 
 print(f"Let's talk about {name}.")
-print(f"He's {height_converted_to_centimeters} inches tall.")
-print(f"He's {weight_converted_to_kilograms} pounds heavy.")
+# print(f"He's {height} inches tall.")
+print(f"He's {height_converted_to_centimeters} centimeters tall.")
+# print(f"He's {weight} pounds heavy.")
+print(f"He's {weight_converted_to_kilograms} kilograms heavy.")
 print("Actually that's not too heavy.")
 print(f"He's got {eyes} eyes and {hair} hair.")
 print(f"His teeth are usually {teeth} depending on the coffee.")
