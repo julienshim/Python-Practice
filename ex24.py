adae0e7f759ce1a333b478390af1a530e0da338f
jerauld <25723428+jerauld@users.noreply.github.com>
2018-09-26 23:59:16 -0700

    corrected typos

--- ex24.py
+++ ex24.py
@@ -6,9 +6,9 @@ poem = """
 \tThelovely world
 with logic so firmly planted
 cannot discern \n the needs of love
-nor comprehend passiong from intuition
+nor comprehend passion from intuition
 and requires an explanation
-\n\t\twhere there is non.
+\n\t\twhere there is none.
 """
 
 print("--------------")
@@ -16,7 +16,7 @@ print(poem)
 print("--------------")
 
 five = 10 - 2 + 3 - 6
-print(f"This shoudl be five: {five}")
+print(f"This should be five: {five}")
 
 def secret_formula(started):
     jelly_beans = started * 500
@@ -29,7 +29,7 @@ start_point = 10000
 beans, jars, crates = secret_formula(start_point)
 
 # remember that this is another way to format a string
-print("With a starting point of: {}".form(start_point))
+print("With a starting point of: {}".format(start_point))
 
 # it's just like with an f"" string
 print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
@@ -37,6 +37,6 @@ print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 start_point = start_point / 10
 
 print("We can also do that this way:")
-formula = secre_formula(start_point)
+formula = secret_formula(start_point)
 # this is an easy way to apply a list to a format string
 print("We'd have {} beans, {} jars, and {} crates.". format(*formula))
