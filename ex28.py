b5dda7c23273bac443adc27a45a4e10a8a75cb1f
jerauld <25723428+jerauld@users.noreply.github.com>
2018-10-10 11:47:00 -0700

    returning from flu ex28

--- ex28.py
+++ ex28.py
@@ -1,19 +1,19 @@
-True and True #1 true 
-False and True #2 false
-1 == 1 and 2 == 1 #3 false
-"test" == "test" #4 true
-1 == 1 or 2 != 1 #5 true
-True and 1 == 1 #6 true
-False and 0 != 0 #7 false
-True or 1 == 1 #8 true
-"test" == "testing" #9 false
-1 != 0 and 2 == 1 #10 false
-"test" != "testing" #11 true
-"test" == 1 #12 false
+True and True #true 
+False and True #false
+1 == 1 and 2 == 1 #false
+"test" == "test" #true
+1 == 1 or 2 != 1 #true
+True and 1 == 1 #true
+False and 0 != 0 #false
+True or 1 == 1 #true
+"test" == "testing" #false
+1 != 0 and 2 == 1 #false
+"test" != "testing" #true
+"test" == 1 #false
 not (True and False) #true
 not (1 == 1 and 0 != 1) #false
-not (10 == 1 or 1000 == 1000) #true
-not (1 != 10 or 3 == 4) #true
+not (10 == 1 or 1000 == 1000) #false
+not (1 != 10 or 3 == 4) #false
 not ("testing" == "testing" and "Zed" == "Cool Guy") #true
 1 == 1 and (not ("testing" == 1 or 1 == 0)) #true
 "chunky" == "bacon" and (not (3 == 4 or 3 == 3)) #false
