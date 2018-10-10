ff5d634c31c7ded3465bd66d3e646bd729681ae7
jerauld <25723428+jerauld@users.noreply.github.com>
2018-10-10 11:34:47 -0700

    returning from flue ex28.py

---
+++ ex28.py
@@ -0,0 +1,20 @@
+True and True #1 true 
+False and True #2 false
+1 == 1 and 2 == 1 #3 false
+"test" == "test" #4 true
+1 == 1 or 2 != 1 #5 true
+True and 1 == 1 #6 true
+False and 0 != 0 #7 false
+True or 1 == 1 #8 true
+"test" == "testing" #9 false
+1 != 0 and 2 == 1 #10 false
+"test" != "testing" #11 true
+"test" == 1 #12 false
+not (True and False) #true
+not (1 == 1 and 0 != 1) #false
+not (10 == 1 or 1000 == 1000) #true
+not (1 != 10 or 3 == 4) #true
+not ("testing" == "testing" and "Zed" == "Cool Guy") #true
+1 == 1 and (not ("testing" == 1 or 1 == 0)) #true
+"chunky" == "bacon" and (not (3 == 4 or 3 == 3)) #false
+3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")) #false
