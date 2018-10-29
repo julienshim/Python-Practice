6b343767f6101507ccdc1e5a822610b1657d79bc
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-10-28 23:43:23 -0700

    corrected typpos

--- ex38.py
+++ ex38.py
@@ -9,7 +9,7 @@ while len(stuff) != 10:
     next_one = more_stuff.pop()
     print("Adding: ", next_one)
     stuff.append(next_one)
-    print(f"There are {len(stuff)}" items now.")
+    print(f"There are {len(stuff)} items now.")
 
 print("There we go: ", stuff)
 
@@ -19,4 +19,4 @@ print(stuff[1])
 print(stuff[-1]) # whoa! fancy
 print(stuff.pop())
 print(' '.join(stuff)) # what? cool!
-print('#'.join(Stuff[3:5])) #super stellar!
+print('#'.join(stuff[3:5])) #super stellar!
