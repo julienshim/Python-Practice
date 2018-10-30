4d54faf508abb486f780ba27daf8d83a43bd3972
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-10-29 22:39:10 -0700

    ex40 part .0

---
+++ ex40.py
@@ -0,0 +1,8 @@
+mystuff = {'apple': "I AM APPLES!"}
+print(mystuff['apple']) # get apple from dict
+
+import ex40_mystuff
+ex40_mystuff.apple() # get apple from the module
+
+# import ex40_mystuff
+print(ex40_mystuff.tangerine) # same thing, it's just variable
