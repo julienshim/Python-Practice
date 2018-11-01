55d0aa9d78774de5e9203837711ed98bd2fa9099
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-10-31 20:45:27 -0700

    ex40 conclude

--- ex40.py
+++ ex40.py
@@ -1,8 +1,45 @@
 mystuff = {'apple': "I AM APPLES!"}
-print(mystuff['apple']) # get apple from dict
+print(mystuff['apple']) # dict style
 
 import ex40_mystuff
-ex40_mystuff.apple() # get apple from the module
+ex40_mystuff.apple() # module style
 
 # import ex40_mystuff
 print(ex40_mystuff.tangerine) # same thing, it's just variable
+
+
+class MyStuff(object):
+
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print("I AM CLASSY APPLES!")
+
+# class style
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
+
+
+
+
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+happy_bday = Song(["Happy birthday to you",
+                    "I don't want to get sued",
+                    "So I'll stop right there"])
+
+bulls_on_parade = Song(["They rally around tha family",
+                        "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
