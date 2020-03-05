21690d75dfbc209370aa5920368f27fc739c8cb9
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-04 20:17:19 -0800

    typos

--- ex5.py
+++ ex5.py
@@ -4,18 +4,18 @@ height = 72 #inches
 height_converted_to_centimeters = round(height * 2.54) #centimeters
 weight = 200 #lbs
 weight_converted_to_kilograms = round (weight * 0.453592) #kilograms
-eyes = 'Brown'
-teeth = 'White'
-hair = 'Black'
+eyes = 'brown'
+teeth = 'white'
+hair = 'black'
 
 print(f"Let's talk about {name}.")
 # print(f"He's {height} inches tall.")
 print(f"He's {height_converted_to_centimeters} centimeters tall.")
 # print(f"He's {weight} pounds heavy.")
 print(f"He's {weight_converted_to_kilograms} kilograms heavy.")
-print("Actually that's not too heavy.")
+print("Actually, that's not too heavy.")
 print(f"He's got {eyes} eyes and {hair} hair.")
-print(f"His teeth are usually {teeth} depending on the coffee.")
+print(f"His teeth are usually {teeth}, depending on the coffee.")
 
 #this line is tricky, try to get it exactly right
 total = age + height_converted_to_centimeters + weight_converted_to_kilograms
