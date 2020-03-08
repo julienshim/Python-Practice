10dfbd83b992f760c29598594fc3ab7b1a5b0261
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-07 20:24:35 -0800

    FS

---
+++ TMP3B/Coding Exercise 7- Formatting Strings.py
@@ -0,0 +1,9 @@
+first = "Julien"
+last = "S"
+
+# version 3.5
+formatted_35 = "First Name: {}, Last Name: {}".format(first, last)
+print(formatted_35)
+# version 3.6
+formatted_36 = f"First Name: {first}, Last Name: {last}"
+print(formatted_36)
