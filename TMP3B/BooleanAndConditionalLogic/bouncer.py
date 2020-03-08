f76914f71f520f6dd817784c666c72384de8d275
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-08 11:54:49 -0700

    bouncer

---
+++ TMP3B/BooleanAndConditionalLogic/bouncer.py
@@ -0,0 +1,29 @@
+#ask for age
+# print("How old are you?")
+# age = input()
+# if age:
+#   age = int(age)
+#   if age > 18 and age < 21:
+#     # 18-21 wristband
+#     print("You can enter, but need a wristband!")
+#   elif age >= 21:
+#     # 21+ drink, normal entry
+#     print("You are good to enter and can drink!")
+#   else:
+#     # too young, sorry
+#     print("You can't drink, little one!")
+# else:
+#   print("Please enter an age!")
+
+print("How old are you?")
+age = input()
+if age:
+  age = int(age)
+  if age >= 21:
+    print("You are good to enter and can drink!")
+  elif age >= 18:
+    print("You can enter, but need a wristband!")
+  else:
+    print("You can't drink, little one!")
+else:
+  print("Please enter an age!")
