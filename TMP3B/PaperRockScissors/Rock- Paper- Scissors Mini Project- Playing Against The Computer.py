1fe442a27eb05bbc5636ca0ea3f0cfeb2d44a720
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-10 14:43:00 -0700

    Create Rock- Paper- Scissors Mini Project- Playing Against The Computer.py
    playing against the computer rps mini project initial

---
+++ TMP3B/PaperRockScissors/Rock- Paper- Scissors Mini Project- Playing Against The Computer.py
@@ -0,0 +1,46 @@
+from random import choice 
+
+movesList = ['rock', 'paper', 'scissors']
+
+validMoves = {
+  "paper": True,
+  "rock": True,
+  "scissors": True
+}
+
+# just felt like looping
+for move in movesList:
+  print(f'...{move}...')
+
+player = input("Player's choice: ").lower()
+# not sure why randomint was recommended when choice removes a lot of steps.
+computer = choice(movesList)
+
+# impromtu validation
+if not player in validMoves:
+  player = False
+
+if not computer in validMoves:
+  computer = False
+
+print("SHOOT!")
+
+def result(win_res, los_res, winner):
+  win_res = win_res.lower().capitalize()
+  los_res = los_res.lower()
+  print(f'{win_res} beats {los_res}. {winner} wins!')
+
+
+if player and computer:
+  if player == computer:
+    print("It's a tie!")
+  elif player == "rock" and computer == "scissors":
+    result(player, computer, "Player")
+  elif player == "paper" and computer == "rock":
+    result(player, computer, "Player")
+  elif player == "scissors" and computer == "paper":
+    result(player, computer, "Player")
+  else:
+    result(computer, player, "Computer")
+else: 
+  print("Uh oh, something went wrong!")
