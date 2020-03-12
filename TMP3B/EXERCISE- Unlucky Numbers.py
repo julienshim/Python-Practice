084108c96a3811a2cef354610fddef7d1dc06799
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-12 15:13:38 -0700

    Update EXERCISE- Unlucky Numbers.py
    refactor

--- TMP3B/EXERCISE- Unlucky Numbers.py
+++ TMP3B/EXERCISE- Unlucky Numbers.py
@@ -1,7 +1,16 @@
+# for num in range(1, 21):
+#     if num == 4 or num == 13:
+#         print(f'{num} is UNLUCKY!')
+#     elif num % 2 == 1:
+#         print(f'{num} is odd')
+#     else:
+#         print(f'{num} is even')
+
 for num in range(1, 21):
     if num == 4 or num == 13:
-        print(f'{num} is UNLUCKY!')
+        state = 'UNLUCKY!'
     elif num % 2 == 1:
-        print(f'{num} is odd')
+        state = 'odd'
     else:
-        print(f'{num} is even')
+        state = 'even'
+    print(f'{num} is {state}')
