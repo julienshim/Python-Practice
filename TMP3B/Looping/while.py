27c872707aa2fb8bc0aafb0d6cd6356a04259d50
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-12 20:26:08 -0700

    move

---
+++ TMP3B/Looping/while.py
@@ -0,0 +1,15 @@
+# message = input('What\'s the secret password? ')
+
+# while message != 'bananas':
+#   print('Wrong! Guess again.')
+#   message = input('What\'s the secret password? ')
+# print('That\'s correct!')
+
+# for num in range(1,11):
+#   print(num)
+
+
+num = 1
+while num < 11:
+  print(num)
+  num += 1
