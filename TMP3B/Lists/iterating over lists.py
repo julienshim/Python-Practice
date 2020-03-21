922c9a93600d629b6e7d629094f2b0d864fad264
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-20 21:54:18 -0700

    iterating notes

---
+++ TMP3B/Lists/iterating over lists.py
@@ -0,0 +1,15 @@
+# colors = ['purple', 'teal', 'magenta']
+
+# for color in colors:
+#     print(color)
+
+numbers = [4, 6, 2, 9, 7, 10]
+
+for num in numbers:
+    print(num * num)
+
+i = 0
+
+while i < len(numbers):
+    print(numbers[i])
+    i += 1
