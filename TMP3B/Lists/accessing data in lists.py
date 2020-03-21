a66791fb0f61f8746319d9e583c40b420ab13d49
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-20 21:54:02 -0700

    accessing data notes

---
+++ TMP3B/Lists/accessing data in lists.py
@@ -0,0 +1,8 @@
+colors = ['purple', 'teal', 'orange']
+
+the_best = colors[0]
+print(the_best)
+the_last = colors[-1]
+print(the_last)
+if 'purple' in colors:
+    print('purple exists in the colors list')
