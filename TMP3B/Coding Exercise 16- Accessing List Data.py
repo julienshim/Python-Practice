4ed55daba4a74044089d79b7ea897d9d6950cf36
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-20 21:43:55 -0700

    coding exercise 16

---
+++ TMP3B/Coding Exercise 16- Accessing List Data.py
@@ -0,0 +1,10 @@
+# DON'T TOUCH THIS PLEASE!
+people = ["Hanna", "Louisa", "Claudia", "Angela", "Geoffrey", "aparna"]
+# DON'T TOUCH THIS PLEASE!
+
+# Change "Hanna" to "Hannah"
+people[0] = "Hannah"
+# Change "Geoffrey" to "Jeffrey"
+people[-2] = "Jeffrey"  # or people[4]
+# Change "aparna" to "Aparna" (capitalize it)
+people[-1] = people[-1].capitalize()  # or 'Aparna' manually
