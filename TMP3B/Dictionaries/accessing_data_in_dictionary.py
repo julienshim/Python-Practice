830f4ee0534b5702a642ffe7efe9b95cbf7af85e
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-25 15:51:21 -0700

    accessing data in dict

---
+++ TMP3B/Dictionaries/accessing_data_in_dictionary.py
@@ -0,0 +1,20 @@
+# character = {
+#     "name": "Harry",
+#     "owns_dog": True,
+#     "num_spells": 34,
+#     "fav_lang": "French",
+#     "is_top10_in_class": True,
+#     "fav_num": 54
+# }
+
+# print(character['name'])  # Harry
+# print(character['thingamajig'])  # KeyError
+
+cat = {
+    "name": "blue",
+    "age": 3.5,
+    "isCute": True,
+}
+
+prop = 'isCute'
+print(cat[prop])
