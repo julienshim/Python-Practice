1707c6b9edbb1f3fa3f654b0a740f6709633363f
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-31 22:01:02 -0700

    using in with dict

---
+++ TMP3B/Dictionaries/using_in_with_dictionaries.py
@@ -0,0 +1,13 @@
+character = {
+    "name": "Harry",
+    "owns_dog": True,
+    "num_spells": 34,
+    "fav_lang": "French",
+    "is_top10_in_class": True,
+    "fav_num": 54
+}
+
+print('name' in character)
+print('blah' in character)
+print(34 in character.values())
+print(False in character.values())
