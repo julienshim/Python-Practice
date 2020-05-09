8361b4a54f2bdb76884d84170fa7a57eafb9e4d1
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-05-09 10:52:46 -0700

    alt ce34 solv

--- TMP3B/Coding Exercise 34- Vowels Dict Exercise.py
+++ TMP3B/Coding Exercise 34- Vowels Dict Exercise.py
@@ -1,2 +1,4 @@
 # make sure your solution is assigned to the answer variable so the tests can work! # noqa
 answer = {k: 0 for k in ['a', 'e', 'i', 'o', 'u']}
+# alt 1 answer = {char:0 for char in 'aeiou'}
+# alt 2 answer = dict.fromkeys("aeiou", 0)
