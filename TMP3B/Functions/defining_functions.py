41cb9a7e810e58da687abf6bb3720b019cdeb6cf
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-05-13 20:04:48 -0700

    def functions

---
+++ TMP3B/Functions/defining_functions.py
@@ -0,0 +1,18 @@
+def say_hi():
+    print('Hi!')
+
+
+say_hi()
+
+
+def sing_happy_birthday():
+    print('Happy Birthday To You')
+    print('Happy Birthday To You')
+    print('Happy Birthday Dear You')
+    print('Happy Birthday To You')
+
+
+sing_happy_birthday()
+sing_happy_birthday()
+sing_happy_birthday()
+sing_happy_birthday()
