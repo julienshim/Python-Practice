2d55d49f08d8b57a0526dc9a04049274fd752d06
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-05-15 17:57:12 -0700

    return

---
+++ TMP3B/Functions/return.py
@@ -0,0 +1,15 @@
+# def print_square_of_7():
+#     print(7**2)
+
+
+# print_square_of_7()
+
+# nums = [1, 2, 3, 4]
+
+def square_of_7():
+    return 7**2
+
+
+result = square_of_7()
+print(result)
+print(square_of_7())
