e10276e113d3dcf9741e633cc9857622c61902ac
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-05-17 11:30:09 -0700

    parameters

---
+++ TMP3B/Functions/parameters.py
@@ -0,0 +1,24 @@
+def square(num):
+    return num*num
+
+
+# print(square(4))
+# print(square(8))
+
+
+def sing_happy_birthday(name):
+    print('Happy Birthday To You')
+    print('Happy Birthday To You')
+    print(f'Happy Birthday Dear {name}')
+    print('Happy Birthday To You')
+
+
+# sing_happy_birthday('Rashida')
+# sing_happy_birthday('Sam')
+
+def divide(num1, num2):
+    return num1/num2
+
+
+print(divide(2, 5))
+print(divide(5, 2))
