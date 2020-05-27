00166b06c8a666c00980b278d7e60f2ec080e889
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-05-26 18:44:44 -0700

    ce46 number compare

---
+++ TMP3B/CodingExercises/Coding Exercise 46- number_compare.py
@@ -0,0 +1,13 @@
+'''
+number_compare(1,1) # "Numbers are equal"
+number_compare(1,0) # "First is greater"
+number_compare(2,4) # "Second is greater"
+'''
+
+
+def number_compare(a, b):
+    if a > b:
+        return "First is greater"
+    elif b > a:
+        return "Second is greater"
+    return "Numbers are equal"
