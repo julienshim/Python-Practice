171eb9022e68edabdf25f8478cae021a915aec55
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-05-28 20:35:04 -0700

    ce50 is palin

---
+++ TMP3B/CodingExercises/Coding Exercise 50- is_palindrome.py
@@ -0,0 +1,19 @@
+'''
+is_palindrome('testing') # False
+is_palindrome('tacocat') # True
+is_palindrome('hannah') # True
+is_palindrome('robert') # False
+is_palindrome('amanaplanacanalpanama') # True
+'''
+
+
+def is_palindrome(string):
+    strip = string.lower().strip()
+    return strip == strip[::-1]
+
+# alt1 - without whitespace removal
+    # return string == string[::-1]
+
+# alt2
+    # stripped = string.replace(" ", "")
+    # return stripped == stripped[::-1]
