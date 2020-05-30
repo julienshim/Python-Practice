4e65f58d4cacea97628118d4654ce10317c0aa3e
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-05-30 12:57:20 -0700

    alternatives for ce55

--- TMP3B/CodingExercises/Coding Exercise 55- intersection.py
+++ TMP3B/CodingExercises/Coding Exercise 55- intersection.py
@@ -4,3 +4,19 @@
 
 def intersection(la, lb):
     return list(set(la) & set(lb))
+
+# alt - manual looping
+# def intersection(l1, l2):
+#     in_common = []
+#     for val in l1:
+#         if val in l2:
+#             in_common.append(val)
+#     return in_common
+
+# alt list comprehension solution
+# def intersection(l1, l2):
+#     return [val for val in l1 if val in l2]
+
+# alt sets solution
+# def intersection(list1, list2):
+#     return [val for val in set(list1) & set(list2)]
