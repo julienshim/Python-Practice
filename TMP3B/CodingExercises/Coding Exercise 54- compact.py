632db0c02e109aee8abb7ae923df02d4b8b39d6f
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-05-30 12:49:13 -0700

    alternative

--- TMP3B/CodingExercises/Coding Exercise 54- compact.py
+++ TMP3B/CodingExercises/Coding Exercise 54- compact.py
@@ -5,3 +5,10 @@ compact([0,1,2,"",[], False, {}, None, "All done"]) # [1,2, "All done"]
 
 def compact(collection):
     return [item for item in collection if bool(item)]
+
+# alt without list comprehension
+# def compact(l):
+    # truthy_vals = []
+    # for val in l:
+    #     if val: truthy_vals.append(val)
+    # return truthy_vals
