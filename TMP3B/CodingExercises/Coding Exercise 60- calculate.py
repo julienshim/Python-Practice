550c746fde1673b59cf38e4baf2006387ac08849
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-06-06 20:21:18 -0700

    corrected version

--- TMP3B/CodingExercises/Coding Exercise 60- calculate.py
+++ TMP3B/CodingExercises/Coding Exercise 60- calculate.py
@@ -4,14 +4,32 @@ calculate(make_float=True, operation='divide', first=3.5, second=5) # "The resul
 '''
 
 
-def calculate(make_float, operation, first, second, message="The result is"):
-    answer = 0
-    if operation == "add":
-        answer = first + second
-    elif operation == "subtract":
-        answer = first - second
-    elif operation == "multiply":
-        answer = first * second
-    elif operation == "divide":
-        answer = first / second
-    return "{} {}".format(message, float(answer) if make_float else int(answer))  # noqa
+# def calculate(make_float, operation, first, second, message="The result is"):
+#     answer = 0
+#     if operation == "add":
+#         answer = first + second
+#     elif operation == "subtract":
+#         answer = first - second
+#     elif operation == "multiply":
+#         answer = first * second
+#     elif operation == "divide":
+#         answer = first / second
+#     return "{} {}".format(message, float(answer) if make_float else int(answer))  # noqa
+
+
+def calculate(**kwargs):
+    operation_lookup = {
+        'add': kwargs.get('first', 0) + kwargs.get('second', 0),
+        'subtract': kwargs.get('first', 0) - kwargs.get('second', 0),
+        'divide': kwargs.get('first', 0) / kwargs.get('second', 0),
+        'multiply': kwargs.get('first', 0) * kwargs.get('second', 0)
+    }
+    is_float = kwargs.get('make_float', False)
+    operation_value = operation_lookup[kwargs.get('operation', '')]
+    if is_float:
+        final = "{} {}".format(kwargs.get(
+            'message', 'The result is'), float(operation_value))
+    else:
+        final = "{} {}".format(kwargs.get(
+            'message', 'The result is'), int(operation_value))
+    return final
