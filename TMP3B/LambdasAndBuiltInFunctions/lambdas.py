73ee51a28870f1ed4db7580c57c85a173020f426
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-06-07 18:44:23 -0700

    example

--- TMP3B/LambdasAndBuiltInFunctions/lambdas.py
+++ TMP3B/LambdasAndBuiltInFunctions/lambdas.py
@@ -1,5 +1,8 @@
 def square(num): return num * num  # single line function
 
 
-# lambda num: num * num  # anonymous function; only one line
 print(square(9))
+
+# square2 = lambda num: num * num  # anonymous function; only one line
+
+# print(square2(9))
