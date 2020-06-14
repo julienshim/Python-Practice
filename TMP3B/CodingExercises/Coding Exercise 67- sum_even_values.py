d7fe007222ba8fce5bf72704ebd058a85c346861
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-06-14 16:53:09 -0700

    coding exercise 67

---
+++ TMP3B/CodingExercises/Coding Exercise 67- sum_even_values.py
@@ -0,0 +1,11 @@
+'''
+sum_even_values(1,2,3,4,5,6) # 12
+sum_even_values(4,2,1,10) # 16
+sum_even_values(1) # 0
+'''
+
+# define sum_even_values
+
+
+def sum_even_values(*args):
+    return sum([n for n in args if n % 2 == 0], 0)
