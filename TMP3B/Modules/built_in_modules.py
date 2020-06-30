bbac419d09f889ebdb4e067f2a8daea7c5fa9a88
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-06-29 21:23:19 -0700

    build in modules

---
+++ TMP3B/Modules/built_in_modules.py
@@ -0,0 +1,13 @@
+# import random  # as rand (used to shorten long module names)
+
+# print(random.choice(["apple", "banana", "cherry", "durian"]))
+# print(random.randint(1, 100))
+# fruit_list = ["apple", "banana", "cherry", "durian"]
+# print(fruit_list)
+# random.shuffle(fruit_list)
+# print(fruit_list)
+
+from random import choice, randint
+
+print(choice(["apple", "banana", "cherry", "durian"]))
+print(randint(1, 100))
