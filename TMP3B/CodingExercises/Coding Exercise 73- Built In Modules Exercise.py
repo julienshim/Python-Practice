2c6a6c284f1951da4cba0ac4a48b46c2752425be
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-06-29 21:27:26 -0700

    ce73 exercise

---
+++ TMP3B/CodingExercises/Coding Exercise 73- Built In Modules Exercise.py
@@ -0,0 +1,13 @@
+# Import the math module:
+
+from math import sqrt
+
+# Use math.sqrt  to find the square root of 15129 and save it to variable called answer: # noqa
+
+answer = sqrt(15129)
+
+# alt
+
+# import math
+
+# answer = math.sqrt(15129)
