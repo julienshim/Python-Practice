02c4d001251c78f9048f57a7877aaac605e5f8c5
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-06-30 22:04:02 -0700

    Coding Exercise 75

---
+++ TMP3B/CodingExercises/Coding Exercise 75- Custom Module Exercise.py
@@ -0,0 +1,7 @@
+# Import your helpers module here.  Do not use the 'from' or 'as' syntax, just use a plain old 'import' # noqa
+
+import ce_75_helper
+
+# Call the lucky_number function from your helpers module, and save the result to a variable called num # noqa
+
+num = ce_75_helper.lucky_number()
