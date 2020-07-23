1ae75e4a9c333cdd043cd51589ff66360bb65ba1
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-07-22 21:21:06 -0700

    asking and asking 2 loop to keep program running until user stops

--- TMP3B/HTTP/dad_blind.py
+++ TMP3B/HTTP/dad_blind.py
@@ -9,30 +9,57 @@ title_color = "magenta"
 colored_title = colored(title_msg, color=title_color)
 print(colored_title)
 
-# Let me tell you a joke! Give me a topic:
 
 topic = input("Let me tell you a joke! Give me a topic: ")
 
-url = "https://icanhazdadjoke.com/search"
+asking = True
 
-header = {
-    "Accept": "application/json"
-}
+while asking:
 
-param = {
-    "term": topic,
-}
+    # Let me tell you a joke! Give me a topic:
 
-# I've got 4 jokes about snow. Here's one:
+    url = "https://icanhazdadjoke.com/search"
 
-response = get(url, headers=header, params=param)
-data = response.json()
-jokes = data["results"]
+    header = {
+        "Accept": "application/json"
+    }
 
-if jokes:
-    random = choice(jokes)['joke']
-    print(f"I've got {len(jokes)} about {topic}. Here's one: ")
-    print(f"{random}")
-# Sorry, I don't have any jokes about ____! Please try again.
-else:
-    print(f"Sorry, I don't have any jokes about {topic}! Please try again.")
+    param = {
+        "term": topic,
+    }
+
+    # I've got 4 jokes about snow. Here's one:
+
+    response = get(url, headers=header, params=param)
+    data = response.json()
+    jokes = data["results"]
+
+    if jokes:
+        random = choice(jokes)['joke']
+        plural = "joke" if len(jokes) == 1 else "jokes"
+        print(f"I've got {len(jokes)} {plural} about {topic}. Here's one: ")
+        print(f"{random}")
+    # Sorry, I don't have any jokes about ____! Please try again.
+    else:
+        print(
+            f"Sorry, I don't have any jokes about {topic}! Please try again.")
+
+    replay = input(f"Do you want to hear another? (y/n) ").lower()
+
+    asking2 = True
+
+    while asking2:
+
+        if replay == "yes" or replay == "y":
+            topic = input(
+                "Okay! Let me tell you another joke! Give me a topic: ")
+            asking2 = False
+        elif replay == "no" or replay == "n":
+            print("Okay. Okay. Tough crowd!")
+            asking = False
+            asking2 = False
+        else:
+            print("Sorry, I didn't get that.")
+            replay = input(f"Do you want to hear another? (y/n) ")
+
+print("See ya! Bye now.")
