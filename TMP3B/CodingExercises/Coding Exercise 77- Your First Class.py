841685429a165e2fb882a2c61bb6410aa1b4fddc
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-08-04 23:30:56 -0700

    ce77 solve

---
+++ TMP3B/CodingExercises/Coding Exercise 77- Your First Class.py
@@ -0,0 +1,8 @@
+# Define the Comment class below:
+
+
+class Comment:
+    def __init__(self, username, text, likes=0):
+        self.username = username
+        self.text = text
+        self.likes = likes
