f62403a326336a8374a3e95e8e2cff1451d37d14
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-08-17 21:07:52 -0700

    user_class_attributes init

---
+++ TMP3B/OOP/user_class_attributes.py
@@ -0,0 +1,51 @@
+class User:
+
+    active_users = 0
+
+    def __init__(self, first, last, age):
+        self.first = first
+        self.last = last
+        self.age = age
+        User.active_users += 1
+    # always need self
+
+    def logout(self):
+        User.active_users -= 1
+        return f"{self.first} has logged out."
+
+    def full_name(self):
+        return f"{self.first} {self.last}"
+
+    def initials(self):
+        return f"{self.first[0]}.{self.last[0]}."
+
+    def likes(self, thing):
+        return f"{self.first} likes {thing.lower()}."
+
+    def is_senior(self):
+        return self.age > 65
+
+    def birthday(self):
+        self.age += 1
+        end = ""
+        if str(self.age)[-1] == "1":
+            end = "st"
+        elif str(self.age)[-1] == "2":
+            end = "nd"
+        elif str(self.age)[-1] == "3":
+            end = "rd"
+        else:
+            end = "th"
+        return f"Happy {self.age}{end} Birthday, {self.first}!"
+
+
+print(User.active_users)
+
+user1 = User("Joe", "Smith", 68)
+user2 = User("Blanca", "Lopez", 41)
+
+print(User.active_users)
+
+print(user2.logout())
+
+print(User.active_users)
