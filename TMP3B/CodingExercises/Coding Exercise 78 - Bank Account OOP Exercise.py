0b527d364e1c5099e564653376465b95145eb71e
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-08-17 20:35:53 -0700

    notes to compare with sol

--- TMP3B/CodingExercises/Coding Exercise 78 - Bank Account OOP Exercise.py
+++ TMP3B/CodingExercises/Coding Exercise 78 - Bank Account OOP Exercise.py
@@ -4,13 +4,13 @@ class BankAccount:
         self.owner = owner
         self.balance = 0.0
 
-    def deposit(self, deposit):
+    def deposit(self, deposit):  # could have said "amount"
         self.balance += deposit
         return self.balance
 
-    def withdraw(self, withdraw):
+    def withdraw(self, withdraw):  # could have said "amount"
         self.balance -= withdraw
         return self.balance
 
-    def balance(self):
+    def balance(self):  # why does the solution have "getBalance"
         return self.balance
