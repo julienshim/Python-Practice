925d71eea96712a2b46a021f8e3304d12f31319d
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-08-24 20:56:11 -0700

    test samples

--- TMP3B/OOP/Deck of Cards.py/deck_of_cards_blind.py
+++ TMP3B/OOP/Deck of Cards.py/deck_of_cards_blind.py
@@ -1,10 +1,12 @@
 class Card:
+    pass
     # each instance of card should have a suit # noqa
     # each instance of card should have a value  # noqa
     # cards repr method should display the card's value and suit # noqa
 
 
 class Deck:
+    pass
     # each instance of deck should have cards attributed with all 52 possible instances of card # noqa
     # deck should have instance method called count which returns a count of how many cards remain in deck # noqa
     # deck's repr method should display information on how many cards are in the deck # noqa
@@ -12,3 +14,17 @@ class Deck:
     # deck should have an instance method called shuffle
     # deck should have an instance method called deal_card
     # deck should have an instance method called deal_hand
+
+
+# Tests
+# my_deck = Deck()
+# print(my_deck)  # Deck of 52 cards.
+
+# my_deck.shuffle()
+# card = my_deck.deal_card()
+# print(card)  # A of Hearts
+
+# hand = my_deck.deal_hand(5)
+# # [8 of Spades, 10 of Clubs, 6 of Spades, 10 of Diamonds, Q of Spades]
+# print(hand)
+# print(my_deck)  # Deck of 46 cards.
