86efa393b13136159a48ffc4b72d824ac894e2f8
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-08-24 20:52:45 -0700

    deck of cards blind init

---
+++ TMP3B/OOP/Deck of Cards.py/deck_of_cards_blind.py
@@ -0,0 +1,14 @@
+class Card:
+    # each instance of card should have a suit # noqa
+    # each instance of card should have a value  # noqa
+    # cards repr method should display the card's value and suit # noqa
+
+
+class Deck:
+    # each instance of deck should have cards attributed with all 52 possible instances of card # noqa
+    # deck should have instance method called count which returns a count of how many cards remain in deck # noqa
+    # deck's repr method should display information on how many cards are in the deck # noqa
+    # deck should have instance method called _deal which accepts a number and removes at most that many cards from the deck # noqa
+    # deck should have an instance method called shuffle
+    # deck should have an instance method called deal_card
+    # deck should have an instance method called deal_hand
