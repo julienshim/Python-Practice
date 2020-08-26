a01db05bbf9372661d78ed3afa91afb0259c4ad6
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-08-25 18:25:30 -0700

    class deck initial init repr count deal shuffle

--- TMP3B/OOP/Deck of Cards.py/deck_of_cards_blind.py
+++ TMP3B/OOP/Deck of Cards.py/deck_of_cards_blind.py
@@ -1,3 +1,6 @@
+from random import shuffle
+
+
 class Card:
     # Each instance of Card  should have a suit # noqa
     # Each instance of Card  should have a value  # noqa
@@ -12,14 +15,35 @@ class Card:
 
 
 class Deck:
-    pass
-    # each instance of deck should have cards attributed with all 52 possible instances of card # noqa
-    # deck should have instance method called count which returns a count of how many cards remain in deck # noqa
-    # deck's repr method should display information on how many cards are in the deck # noqa
-    # deck should have instance method called _deal which accepts a number and removes at most that many cards from the deck # noqa
-    # deck should have an instance method called shuffle
-    # deck should have an instance method called deal_card
-    # deck should have an instance method called deal_hand
+    # Each instance of Deck  should have a cards attribute with all 52 possible instances of Card . # noqa
+    def __init__(self):
+        self.cards = []
+        for suit in ("Hearts", "Diamonds", "Clubs", "Spades"):
+            for value in ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"):  # noqa
+                self.cards.append(Card(suit, value))
+
+    def __repr__(self):
+        return f"Deck of {self.count()} cards."
+
+    # Deck  should have an instance method called count  which returns a count of how many cards remain in the deck. # noqa
+    def count(self):
+        return len(self.cards)
+
+    # Deck  should have an instance method called _deal  which accepts a number and removes at most that many cards from the deck # noqa
+    def deal(self, number):
+        if self.card:
+            pass
+        else:
+            return ValueError('All cards have been dealt.')
+
+    # Deck  should have an instance method called shuffle  which will shuffle a full deck of cards. # noqa
+        if self.card:
+            self.cards = shuffle(self.cards)
+            return self.cards
+        else:
+            return ValueError('Only full decks can be shuffled"')
+
+    # Deck  should have an instance method called deal_hand  which accepts a number and uses the _deal # noqa
 
 
 # Tests
