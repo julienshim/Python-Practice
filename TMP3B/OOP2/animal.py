2498604ea18a56384d8ae92ce35f62200c4450e7
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-09-13 19:16:02 -0700

    animal py 2

--- TMP3B/OOP2/animal.py
+++ TMP3B/OOP2/animal.py
@@ -1,23 +1,39 @@
 class Animal:
-    cool = True
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+    def __repr__(self):
+        return f"{self.name} is a {self.species}."
 
     def make_sound(Self, sound):
-        print(f"this animal says {sound}")
+        print(f"This animal says '{sound}'.'")
 
 
 class Cat(Animal):
-    pass
+    def __init__(self, name, breed, toy):
+        # Animal.__init__(self, name, species)
+        super().__init__(name, species="cat")
+        self.breed = breed
+        self.toy = toy
+
+    def play(self):
+        print(f"{self.name} plays with {self.toy}")
 
 
 # a = Animal()
 # a.make_sound("CHIRP")
-
-blue = Cat()
+blue = Cat("Blue", "Scottish Fold", "string")
+# print(blue)
+# print(blue.species)
+# print(blue.breed)
+# print(blue.toy)
+blue.play()
 # blue.make_sound("MEOW")
 # print(blue.cool)
 # print(Cat.cool)
 # print(Animal.cool)
 
-print(isinstance(blue, Cat))
-print(isinstance(blue, Animal))
-print(isinstance(blue, object))
+# print(isinstance(blue, Cat))
+# print(isinstance(blue, Animal))
+# print(isinstance(blue, object))
