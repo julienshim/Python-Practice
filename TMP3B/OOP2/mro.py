0b77184d1f83095ad0f286ff7dc545c98af8f373
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-09-18 23:13:31 -0700

    mro

---
+++ TMP3B/OOP2/mro.py
@@ -0,0 +1,26 @@
+class A:
+    def do_something(self):
+        print("Method Defined In: A")
+
+class B(A):
+    # pass
+    def do_something(self):
+        print("Method Defined In: B")
+
+class C(A):
+    # pass
+    def do_something(self):
+        print("Method Defined In: C")
+
+class D(B, C):
+    # pass
+    def do_something(self):
+        print("Method Defined In: D")
+        super().do_something()
+# print(D.__mro__)
+# print(D.mro())
+# help(D)
+thing = D()
+thing.do_something()
+
+# D -> B -> C -> A -> Object
