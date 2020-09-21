4156582ef6c604464cc2fe45ef67b14400e5c092
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-09-21 10:31:48 -0700

    spacing issues

--- TMP3B/OOP2/mro.py
+++ TMP3B/OOP2/mro.py
@@ -2,21 +2,26 @@ class A:
     def do_something(self):
         print("Method Defined In: A")
 
+
 class B(A):
     # pass
     def do_something(self):
         print("Method Defined In: B")
+
 
 class C(A):
     # pass
     def do_something(self):
         print("Method Defined In: C")
 
+
 class D(B, C):
     # pass
     def do_something(self):
         print("Method Defined In: D")
         super().do_something()
+
+
 # print(D.__mro__)
 # print(D.mro())
 # help(D)
