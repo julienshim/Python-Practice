67d7841a09d1be2b01d405149e27f0acb59a145f
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-10-11 23:08:30 -0700

    alternative

--- TMP3B/CodingExercises/Coding Exercise 86- yes_or_no.py
+++ TMP3B/CodingExercises/Coding Exercise 86- yes_or_no.py
@@ -11,3 +11,10 @@ def yes_or_no():
     while True:
         yield 'yes'
         yield 'no'
+
+# alt
+# def yes_or_no():
+#     answer = "yes"
+#     while True:
+#         yield answer
+#         answer = "no" if answer == "yes" else "yes"
