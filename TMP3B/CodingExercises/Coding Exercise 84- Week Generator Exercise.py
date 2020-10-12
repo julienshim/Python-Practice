d7651b248226780688c05a291de4a758b8f24780
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-10-11 22:55:44 -0700

    ce84 blind

---
+++ TMP3B/CodingExercises/Coding Exercise 84- Week Generator Exercise.py
@@ -0,0 +1,20 @@
+'''
+days = week()
+next(days) # 'Monday'
+next(days) # 'Tuesday'
+next(days) # 'Wednesday'
+next(days) # 'Thursday'
+next(days) # 'Friday'
+next(days) # 'Saturday'
+next(days) # 'Sunday'
+next(days) # StopIteration
+'''
+
+
+def week():
+    days = ['Monday', 'Tuesday', 'Wednesday',
+            'Thursday', 'Friday', 'Saturday', 'Sunday']
+    index = 0
+    while index < len(days):
+        yield days[index]
+        index += 1
