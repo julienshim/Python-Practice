9647dadde2d59bbe6e52ecaf94f505d22bf4ad83
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-10-13 12:48:28 -0700

    alternative

--- TMP3B/CodingExercises/Coding Exercise 86- make_song.py
+++ TMP3B/CodingExercises/Coding Exercise 86- make_song.py
@@ -26,3 +26,12 @@ def make_song(count=99, beverage='soda'):
             yield 'No more {}!'.format(beverage)
             running = False
     return StopIteration
+
+# def make_song(verses=99, beverage="soda"): # strange alternative solution that doesn't follow guidelines re: count # noqa
+#     for num in range(verses, -1, -1):
+#         if num > 1:
+#             yield "{} bottles of {} on the wall.".format(num, beverage)
+#         elif num == 1:
+#             yield "Only 1 bottle of {} left!".format(beverage)
+#         else:
+#             yield "No more {}!".format(beverage)
