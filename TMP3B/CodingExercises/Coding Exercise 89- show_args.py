21717107e2610752a64a712ff0679037ef0b5468
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-10-29 19:51:25 -0700

    show_args

---
+++ TMP3B/CodingExercises/Coding Exercise 89- show_args.py
@@ -0,0 +1,27 @@
+'''
+@show_args
+def do_nothing(*args, **kwargs):
+    pass
+
+do_nothing(1, 2, 3,a="hi",b="bye")
+
+# Should print (on two lines):
+# Here are the args: (1, 2, 3)
+# Here are the kwargs: {"a": "hi", "b": "bye"}
+'''
+
+from functools import wraps
+
+
+def show_args(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        print("Here are the args:", args)
+        print("Here are the kwargs:", kwargs)
+        return fn(*args, **kwargs)  # worked without but should return
+    return wrapper
+
+
+@show_args
+def do_nothing(*args, **kwargs):
+    pass
