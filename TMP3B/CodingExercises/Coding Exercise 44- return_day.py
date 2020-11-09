9f0c3273bdc27f591505ea801152c34dbf423cba
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-11-09 10:10:47 -0800

    add doctest

--- TMP3B/CodingExercises/Coding Exercise 44- return_day.py
+++ TMP3B/CodingExercises/Coding Exercise 44- return_day.py
@@ -11,6 +11,24 @@ return_day(41) # None
 
 
 def return_day(n):
+    """
+    >>> return_day(1)
+    'Sunday'
+    >>> return_day(2)
+    'Monday'
+    >>> return_day(3)
+    'Tuesday'
+    >>> return_day(4)
+    'Wednesday'
+    >>> return_day(5)
+    'Thursday'
+    >>> return_day(6)
+    'Friday'
+    >>> return_day(7)
+    'Saturday'
+    >>> return_day(41) is None
+    True
+    """
     days = {
         1: "Sunday",
         2: "Monday",
