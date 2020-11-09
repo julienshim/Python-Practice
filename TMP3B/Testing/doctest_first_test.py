d54918dcce6da6cb7abe6e7cf9c8cebbc0994cd0
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-11-08 21:42:58 -0800

    first doctest

---
+++ TMP3B/Testing/doctest_first_test.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    """
+    >>> add(2, 3)
+    5
+    >>> add(100, 200)
+    300
+    """
+    # return a * b
+    return a + b
+
+# python -m doctest -v <file> (name cannot be doctest)
