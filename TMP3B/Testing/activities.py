dd47c9b6dcdb73b08e0d7a9eb502181f782cba03
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-11-12 18:15:18 -0800

    mod def eat for valueerror raise

--- TMP3B/Testing/activities.py
+++ TMP3B/Testing/activities.py
@@ -1,14 +1,9 @@
 def eat(food, is_healthy):
+    if not isinstance(is_healthy, bool):
+        raise ValueError("is_healthy must be a boolean")
     if is_healthy:
         return f"I'm eating {food}, because my body is a temple."
     return f"I'm eating {food}, because YOLO!"
-
-
-# instructor alt
-# ending = "because YOLO."
-# if is_healthy:
-#   ending = "because my body is a temple."
-# return f"I'm eating {food}, {ending}"
 
 
 def nap(num_hours):
