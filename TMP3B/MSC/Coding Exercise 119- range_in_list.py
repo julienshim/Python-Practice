27dfd096b7af0add90849554218aa109224bbdb3
julienshim <25723428+julienshim@users.noreply.github.com>
2021-04-28 17:27:53 -0700

    ce 119

---
+++ TMP3B/MSC/Coding Exercise 119- range_in_list.py
@@ -0,0 +1,19 @@
+'''
+range_in_list([1,2,3,4],0,2) #  6
+range_in_list([1,2,3,4],0,3) # 10
+range_in_list([1,2,3,4],1) #  9
+range_in_list([1,2,3,4]) # 10
+range_in_list([1,2,3,4],0,100) # 10
+range_in_list([],0,1) # 0
+'''
+
+def range_in_list(arr=[], start=0, end=''):
+    end = len(arr) if end == '' else end + 1
+    return sum(arr[start:end])
+
+print(range_in_list([1,2,3,4],0,2)) #  6
+print(range_in_list([1,2,3,4],0,3)) # 10
+print(range_in_list([1,2,3,4],1)) #  9
+print(range_in_list([1,2,3,4])) # 10
+print(range_in_list([1,2,3,4],0,100)) # 10
+print(range_in_list([],0,1)) # 0
