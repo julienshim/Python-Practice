9eb386bccc3fc592a0f01c8c1b8746f55be103d7
julienshim <25723428+julienshim@users.noreply.github.com>
2021-04-30 23:45:17 -0700

    inst solve

--- TMP3B/MSC/Coding Exercise 122- find_the_duplicate.py
+++ TMP3B/MSC/Coding Exercise 122- find_the_duplicate.py
@@ -8,3 +8,16 @@ def find_the_duplicate(arr):
     for i in range(len(arr)):
         if arr.count(arr[i]) > 1:
             return arr[i]
+
+# instructor solution
+
+# def find_the_duplicate(arr):
+#     counter = {}
+#     for val in arr:
+#         if val in counter:
+#             counter[val] += 1
+#         else:
+#             counter[val] = 1
+#     for key in counter.keys():
+#         if counter[key] > 1:
+#             return int(key)
