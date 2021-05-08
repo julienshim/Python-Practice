f6447c39ce5733018d55958ece49b3774c9db810
julienshim <25723428+julienshim@users.noreply.github.com>
2021-05-08 12:44:17 -0700

    alt

--- TMP3B/CodingExercises/Coding Exercise 95- copy.py
+++ TMP3B/CodingExercises/Coding Exercise 95- copy.py
@@ -8,3 +8,13 @@ def copy(input_file, output_file):
         copy = input_file.read()
         with open(output_file, 'w') as outputfile:
             outputfile.write(copy)
+
+
+# alt
+
+# def copy(file_name, new_file_name):
+#     with open(file_name) as file:
+#         text = file.read()
+    
+#     with open(new_file_name, "w") as new_file:
+#         new_file.write(text)
