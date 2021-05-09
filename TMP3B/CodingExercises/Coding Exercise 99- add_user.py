e452afaacc677b0ccdb0d34e7cd1fd7edebea91f
julienshim <25723428+julienshim@users.noreply.github.com>
2021-05-08 20:51:27 -0700

    alt

--- TMP3B/CodingExercises/Coding Exercise 99- add_user.py
+++ TMP3B/CodingExercises/Coding Exercise 99- add_user.py
@@ -13,3 +13,12 @@ def add_user(first_name, last_name):
     with open('users.csv', 'a') as file:
         csv_writer = writer(file)
         csv_writer.writerow([first_name, last_name])
+
+# alt
+
+# import csv
+    
+# def add_user(first_name, last_name):
+#     with open("users.csv", "a") as csvfile:
+#         csv_writer = csv.writer(csvfile)
+#         csv_writer.writerow([first_name, last_name])
