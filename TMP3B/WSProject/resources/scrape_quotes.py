eba68fe66212ec0ec1507d4695a7d7571799bacc
julienshim <25723428+julienshim@users.noreply.github.com>
2021-05-15 14:27:17 -0700

    break out to scrape quotes, fruther break into gs, npe, gdt, gpd, pp, and sq

---
+++ TMP3B/WSProject/resources/scrape_quotes.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+from random import randint
+
+from resources.write_to_csv import write_to_csv
+
+def get_soup(page_number):
+    url = f"https://quotes.toscrape.com/page/{page_number}/"
+    response = requests.get(url)
+    return BeautifulSoup(response.text, "html.parser")
+
+def next_page_exists(page_number):
+    soup = get_soup(page_number)
+    return bool(soup.find(class_="next"))
+
+def get_div_text(div):
+    quote = div.find(class_="text").get_text()
+    author = div.find(class_="author").get_text()
+    link = div.find("a")["href"]
+    return [quote, author, link]
+
+def get_page_divs(page_number):
+    soup = get_soup(page_number)
+    quote_divs = soup.find_all(class_="quote")
+    for div in quote_divs:
+        write_to_csv('a', get_div_text(div))
+
+def process_page(current_page):
+    print("*" * current_page)
+    get_page_divs(current_page)
+    sleep(randint(3,8))
+
+def scrape_quotes():
+    current_page = 0
+    while next_page_exists(current_page):
+        current_page += 1
+        process_page(current_page)
