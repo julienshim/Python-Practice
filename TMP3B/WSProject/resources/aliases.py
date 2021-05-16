0eb8eb97c63147cddbc289d947be450d32a0edf4
julienshim <25723428+julienshim@users.noreply.github.com>
2021-05-16 16:14:32 -0700

    aliases init

---
+++ TMP3B/WSProject/resources/aliases.py
@@ -0,0 +1,5 @@
+aliases = {
+    'E.E. Cummings': ['Edward Estlin Cummings', 'Mr. Cummings'],
+    'Eleanor Roosevelt': ['Anna Eleanor Roosevelt'],
+    'Charles Bukowski': ['Henry Charles Bukowski']
+}
