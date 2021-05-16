5ef881ca78111b59bc86bcaafd25e40bfbde3558
julienshim <25723428+julienshim@users.noreply.github.com>
2021-05-16 16:17:05 -0700

    break out get soup to get quote soup

--- TMP3B/WSProject/resources/scrape_quotes.py
+++ TMP3B/WSProject/resources/scrape_quotes.py
@@ -1,17 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
+
 from time import sleep
 from random import randint
 
 from resources.write_to_csv import write_to_csv
-
-def get_soup(page_number):
-    url = f"https://quotes.toscrape.com/page/{page_number}/"
-    response = requests.get(url)
-    return BeautifulSoup(response.text, "html.parser")
-
+from resources.get_soup import get_quote_soup
+ 
 def next_page_exists(page_number):
-    soup = get_soup(page_number)
+    soup = get_quote_soup(page_number)
     return bool(soup.find(class_="next"))
 
 def get_div_text(div):
@@ -21,7 +16,7 @@ def get_div_text(div):
     return [quote, author, link]
 
 def get_page_divs(page_number):
-    soup = get_soup(page_number)
+    soup = get_quote_soup(page_number)
     quote_divs = soup.find_all(class_="quote")
     for div in quote_divs:
         write_to_csv('a', get_div_text(div))
