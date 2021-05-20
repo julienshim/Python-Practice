bc66f0ecd8182c5960c36c266e87035804b61553
julienshim <25723428+julienshim@users.noreply.github.com>
2021-05-19 17:21:55 -0700

    remove targeting  responsibilities

--- TMP3B/WSProject/resources/load_resources.py
+++ TMP3B/WSProject/resources/load_resources.py
@@ -1,6 +1,12 @@
 from csv import reader
+from resources.class_quote import Quote
+from random import shuffle
+
+banned_authors = ['/author/Stephenie-Meyer']
 
 def load_resources():
     with open('resources/csv/blog_data_full_project.csv') as csv_file:
         csv_reader = reader(csv_file)
-        return [d for d in csv_reader]
+        data = [Quote(quote, author, link) for (quote, author, link) in csv_reader][1:]
+        # shuffle(data)
+        return data
