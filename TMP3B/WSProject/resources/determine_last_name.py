9d0efce99281712957314b90ae765561be5e5b54
julienshim <25723428+julienshim@users.noreply.github.com>
2021-05-19 17:18:03 -0700

    import sub bring in arr split

--- TMP3B/WSProject/resources/determine_last_name.py
+++ TMP3B/WSProject/resources/determine_last_name.py
@@ -1,4 +1,7 @@
-def determine_last_name(arr):
+from re import sub
+
+def determine_last_name(name):
+    arr = sub('-', ' ', name).split(' ')
     if '.' not in arr[len(arr)-1] and arr[len(arr)-1][0].lower() != arr[len(arr)-1][0]:
         return arr[len(arr)-1]
     return arr[len(arr)-2]
