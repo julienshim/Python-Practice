db8ae01b87330ea30e4de2f1350f1ec7db9b6ae7
julienshim <25723428+julienshim@users.noreply.github.com>
2021-05-19 17:20:15 -0700

    because the data is scraped is to bad, only will account for x authors on 5/19

--- TMP3B/WSProject/resources/scrape_quotes.py
+++ TMP3B/WSProject/resources/scrape_quotes.py
@@ -3,6 +3,58 @@ from random import randint
 
 from resources.write_to_csv import write_to_csv
 from resources.get_soup import get_quote_soup
+
+targeted_authors =[
+    "/author/Albert-Einstein",
+    "/author/J-K-Rowling",
+    "/author/Jane-Austen",
+    "/author/Marilyn-Monroe",
+    "/author/Andre-Gide",
+    "/author/Thomas-A-Edison",
+    "/author/Eleanor-Roosevelt",
+    "/author/Steve-Martin",
+    "/author/Bob-Marley",
+    "/author/Dr-Seuss",
+    "/author/Douglas-Adams",
+    "/author/Elie-Wiesel",
+    "/author/Friedrich-Nietzsche",
+    "/author/Mark-Twain",
+    "/author/Allen-Saunders",
+    "/author/Pablo-Neruda",
+    "/author/Ralph-Waldo-Emerson",
+    "/author/Mother-Teresa",
+    "/author/Garrison-Keillor",
+    "/author/Jim-Henson",
+    "/author/Charles-M-Schulz",
+    "/author/William-Nicholson",
+    "/author/Jorge-Luis-Borges",
+    "/author/George-Eliot",
+    "/author/George-R-R-Martin",
+    "/author/C-S-Lewis",
+    "/author/Martin-Luther-King-Jr",
+    "/author/James-Baldwin",
+    "/author/Haruki-Murakami",
+    "/author/Alexandre-Dumas-fils",
+    "/author/Ernest-Hemingway",
+    "/author/Helen-Keller",
+    "/author/George-Bernard-Shaw",
+    "/author/Charles-Bukowski",
+    "/author/Suzanne-Collins",
+    "/author/J-R-R-Tolkien",
+    "/author/Alfred-Tennyson",
+    "/author/Terry-Pratchett",
+    "/author/J-D-Salinger",
+    "/author/George-Carlin",
+    "/author/John-Lennon",
+    "/author/W-C-Fields",
+    "/author/Ayn-Rand",
+    "/author/Jimi-Hendrix",
+    "/author/J-M-Barrie",
+    "/author/E-E-Cummings",
+    "/author/Khaled-Hosseini",
+    "/author/Harper-Lee",
+    "/author/Madeleine-LEngle"
+    ]
  
 def next_page_exists(page_number):
     soup = get_quote_soup(page_number)
@@ -18,10 +70,10 @@ def get_page_divs(page_number):
     soup = get_quote_soup(page_number)
     quote_divs = soup.find_all(class_="quote")
     for div in quote_divs:
-        write_to_csv('a', get_div_text(div))
+        if div.find("a")["href"] in targeted_authors:
+            write_to_csv('a', get_div_text(div))
 
 def process_page(current_page):
-    print("*" * current_page)
     get_page_divs(current_page)
     sleep(randint(3,8))
 
