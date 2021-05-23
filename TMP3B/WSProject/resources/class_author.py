05c86b4feaf6d3dde930f3e6a93e0aa0e39a6e74
julienshim <25723428+julienshim@users.noreply.github.com>
2021-05-23 11:24:06 -0700

    expanded and all

--- TMP3B/WSProject/resources/class_author.py
+++ TMP3B/WSProject/resources/class_author.py
@@ -4,14 +4,26 @@ from resources.determine_last_name import determine_last_name
 class Author:
     def __init__(self, name, born_date, born_location, description_arr):
         self.name = name
-        self.born_date = f"This person was born on {born_date}."
-        self.born_location = f"This person was born {born_location}."
+        self.born_date = born_date
+        self.born_location = born_location
         self.description_arr = description_arr
-
-    def get_random_fact(self):
-        len_desc_arr = len(self.description_arr)-1
-        return self.description_arr[randint(0, len_desc_arr)]
 
     def get_last_name_hint(self):
         last_name = determine_last_name(self.name)
         return last_name[0] + len(last_name[1:]) * "*"
+    
+    def get_born_date_hint(self):
+        return f"This person was born on {self.born_date}."
+
+    def get_born_location_hint(self):
+        return f"This person was born {self.born_location}."
+
+    def get_random_bio_fact(self):
+        len_desc_arr = len(self.description_arr)-1
+        return self.description_arr[randint(0, len_desc_arr)]
+
+    def get_all_hints(self):
+        return [self.get_last_name_hint(), 
+                self.get_born_date_hint(),
+                self.get_born_location_hint(),
+                ] + self.description_arr
