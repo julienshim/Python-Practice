4c89d2980f921a611a20efd45e14cf99869318b0
julienshim <25723428+julienshim@users.noreply.github.com>
2021-05-29 14:38:01 -0700

    more clear naming

--- TMP3B/WSProject/resources/class_quote.py
+++ TMP3B/WSProject/resources/class_quote.py
@@ -1,5 +1,5 @@
 class Quote:
-    def __init__(self, quote, author, link):
-        self.text = quote
-        self.author = author
-        self.link = f"https://quotes.toscrape.com{link}"
+    def __init__(self, quote, quote_author, quote_author_bio_link):
+        self.quote = quote
+        self.quote_author = quote_author
+        self.quote_author_bio_link = f"https://quotes.toscrape.com{quote_author_bio_link}"
