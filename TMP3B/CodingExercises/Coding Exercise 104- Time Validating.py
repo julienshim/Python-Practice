a2aa0b1566a709ea9a42ba4dfba4239dc71122cb
julienshim <25723428+julienshim@users.noreply.github.com>
2021-06-25 15:49:26 -0700

    ce104

--- TMP3B/CodingExercises/Coding Exercise 104- Time Validating.py
+++ TMP3B/CodingExercises/Coding Exercise 104- Time Validating.py
@@ -7,3 +7,15 @@ def is_valid_time(input):
     if match:
         return True
     return False
+
+
+## instructor solution
+
+# import re 
+    
+# def is_valid_time(input):
+#     pattern = re.compile(r'^\d\d?:\d\d$')
+#     match = pattern.search(input)
+#     if match:
+#         return True
+#     return False
