e1b813cda72a36426051f61a695a4ed5afac6bc8
julienshim <25723428+julienshim@users.noreply.github.com>
2021-06-25 17:18:22 -0700

    instructor solve

--- TMP3B/CodingExercises/Coding Exercise 107- Regex Profanity.py
+++ TMP3B/CodingExercises/Coding Exercise 107- Regex Profanity.py
@@ -3,3 +3,11 @@ from re import compile, sub, I
 def censor(input):
     pattern = compile(r'frack(er|ing)?', I)
     return pattern.sub('CENSORED', input)
+
+## instructor solution
+
+# import re
+     
+# def censor(input):
+#     pattern = re.compile(r'\bfrack\w*\b', re.IGNORECASE)
+#     return pattern.sub("CENSORED", input)
