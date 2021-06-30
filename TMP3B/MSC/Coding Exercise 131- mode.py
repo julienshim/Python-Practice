472d7c8cc0b2b88c04a3a09460fde40563be9fce
julienshim <25723428+julienshim@users.noreply.github.com>
2021-06-30 11:30:46 -0700

    ce131

---
+++ TMP3B/MSC/Coding Exercise 131- mode.py
@@ -0,0 +1,19 @@
+'''
+mode([2,4,1,2,3,3,4,4,5,4,4,6,4,6,7,4]) # 4
+'''
+
+# define mode below:
+def mode(l):
+    freq = { k:l.count(k) for k in set(l) }
+    return max(freq, key=freq.get)
+
+# instructor
+
+# def mode(collection):
+#     count = {val: collection.count(val) for val in collection}
+#     # find the highest value (the most frequent number)
+#     max_value = max(count.values())
+#     # now we need to see at which index the highest value is at
+#     correct_index = list(count.values()).index(max_value)
+#     # finally, return the correct key for the correct index (we have to convert cou)
+#     return list(count.keys())[correct_index]
