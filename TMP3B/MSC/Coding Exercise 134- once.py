790a0133f6fd948eaf701a335a612398c093707e
julienshim <25723428+julienshim@users.noreply.github.com>
2021-07-01 10:35:31 -0700

    ce134

---
+++ TMP3B/MSC/Coding Exercise 134- once.py
@@ -0,0 +1,28 @@
+'''
+def add(a,b):
+    return a+b
+
+oneAddition = once(add)
+
+oneAddition(2,2) # 4
+oneAddition(2,2) # None
+oneAddition(12,200) # None
+'''
+
+def once(a):
+    a.ran_once = False
+    def inner(b, c):
+        if not a.ran_once:
+            a.ran_once = True
+            return a(b,c)
+    return inner
+
+# instructor
+
+# def once(fn):
+#     fn.is_called = False
+#     def inner(*args):
+#         if not(fn.is_called):
+#             fn.is_called = True
+#             return fn(*args)
+#     return inner
