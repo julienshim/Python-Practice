032899e06f4e681b30439e7b8c5bc71f33b8939c
julienshim <25723428+julienshim@users.noreply.github.com>
2021-07-01 10:35:50 -0700

    ce135

---
+++ TMP3B/MSC/Coding Exercise 135- Next Prime Generator.py
@@ -0,0 +1,31 @@
+'''
+primes = next_prime()
+[next(primes) for i in range(25)]
+# [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+'''
+
+def next_prime():
+    def is_prime(n):
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+    i = 2
+    while True:
+        if is_prime(i):
+            yield i
+        i += 1
+        
+# instructor
+
+# def next_prime():
+#     num = 2
+#     all_primes = set()
+#     while True:
+#         for prime in all_primes:
+#             if num % prime == 0:
+#                 break
+#         else:
+#             all_primes.add(num)
+#             yield num
+#         num += num % 2 + 1
