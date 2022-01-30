2366976945f9b06116813ff3eabc17d464bd06f5
julienshim <25723428+julienshim@users.noreply.github.com>
2022-01-29 22:57:04 -0800

    audio playback init

---
+++ Scripts/audio_playback.py
@@ -0,0 +1,6 @@
+from os import listdir, getcwd
+from playsound import playsound
+from time import sleep
+from datetime import datetime, timedelta
+from re import findall
+from mutagen.wave import WAVE
